11b5ece9bd6676dd04ce4da2445f9d53550aba3c
santiago <santiago.palacio2995@gmail.com>
2026-05-31 19:00:00 -0500

    atualizacion del menu

--- Menu.java
+++ Menu.java
@@ -1,58 +1,96 @@
-
+import modelos.EstudianteIngenieria;
+import modelos.EstudianteDiseno;
 import validaciones.Validaciones;
-
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Menu {
 
-  
-    public static void menuIngenieria() {
-        int opcion;
-        do {
-            System.out.println("\n");
-            System.out.println(" ESTUDIANTES DE INGENIERÍA ");
-            System.out.println(" ");
-            System.out.println(" 1. Registrar préstamo");
-            System.out.println(" 2. Modificar préstamo");
-            System.out.println(" 3. Devolución de equipo ");
-            System.out.println(" 4. Buscar préstamo ");
-            System.out.println(" 5. Volver al menú principal");
-            System.out.println(" ");
+    public static void menuIngenieria(Scanner sc) {
+        GestionIngenieria g = new GestionIngenieria();
+        Validaciones v = new Validaciones();
 
-            opcion = Validaciones.leerOpcionMenu("  Seleccione una opción: ", 1, 5);
+        LinkedList<EstudianteIngenieria> lista = new LinkedList<>();
+        Stack<EstudianteIngenieria>      pila  = new Stack<>();
+        Queue<EstudianteIngenieria>      cola  = new LinkedList<>();
 
-            switch (opcion) {
-                case 1: GestionIngenieria.registrarPrestamo(); break;
-                case 2: GestionIngenieria.modificarPrestamo(); break;
-                case 3: GestionIngenieria.devolverEquipo();    break;
-                case 4: GestionIngenieria.buscarPrestamo();    break;
-                case 5: System.out.println("  Volviendo al menú principal..."); break;
+        boolean bandera = true;
+        while (bandera) {
+            System.out.println("--- ESTUDIANTES DE INGENIERIA ---");
+            System.out.println("1. Llenar lista");
+            System.out.println("2. Llenar pila");
+            System.out.println("3. Llenar cola");
+            System.out.println("4. Mostrar lista");
+            System.out.println("5. Mostrar pila");
+            System.out.println("6. Mostrar cola");
+            System.out.println("7. Modificar");
+            System.out.println("8. Eliminar");
+            System.out.println("9. Buscar por cedula");
+            System.out.println("10. Buscar por serial");
+            System.out.println("11. Volver al menu principal");
+
+            int opt = v.ValidarEntero(sc);
+            opt = v.ValidarRango(1, 11, opt, sc);
+
+            switch (opt) {
+                case 1:  lista = g.llenarLista(lista, sc);      break;
+                case 2:  pila  = g.llenarPila(pila, sc);        break;
+                case 3:  cola  = g.llenarCola(cola, sc);        break;
+                case 4:  g.mostrarLista(lista);                  break;
+                case 5:  g.mostrarPila(pila);                    break;
+                case 6:  g.mostrarCola(cola);                    break;
+                case 7:  lista = g.modificar(lista, sc);         break;
+                case 8:  lista = g.eliminar(lista, sc);          break;
+                case 9:  g.buscar(lista, sc);                    break;
+                case 10: g.buscarPorSerial(lista, sc);           break;
+                case 11: bandera = false;                        break;
+                default: System.out.println("Opcion no valida"); break;
             }
-        } while (opcion != 5);
+        }
     }
 
-    
-    public static void menuDiseno() {
-        int opcion;
-        do {
-           
-            System.out.println("   ESTUDIANTES DE DISEÑO ");
-            System.out.println("  ");
-            System.out.println("  1. Registrar préstamo ");
-            System.out.println("  2. Modificar préstamo ");
-            System.out.println("  3. Devolución de equipo");
-            System.out.println("  4. Buscar préstamo ");
-            System.out.println("  5. Volver al menú principal");
-            System.out.println(" ");
+    public static void menuDiseno(Scanner sc) {
+        GestionDiseno g = new GestionDiseno();
+        Validaciones v = new Validaciones();
 
-            opcion = Validaciones.leerOpcionMenu("  Seleccione una opción: ", 1, 5);
+        LinkedList<EstudianteDiseno> lista = new LinkedList<>();
+        Stack<EstudianteDiseno>      pila  = new Stack<>();
+        Queue<EstudianteDiseno>      cola  = new LinkedList<>();
 
-            switch (opcion) {
-                case 1: GestionDiseno.registrarPrestamo(); break;
-                case 2: GestionDiseno.modificarPrestamo(); break;
-                case 3: GestionDiseno.devolverEquipo();    break;
-                case 4: GestionDiseno.buscarPrestamo();    break;
-                case 5: System.out.println("  ↩ Volviendo al menú principal..."); break;
+        boolean bandera = true;
+        while (bandera) {
+            System.out.println("--- ESTUDIANTES DE DISENO ---");
+            System.out.println("1. Llenar lista");
+            System.out.println("2. Llenar pila");
+            System.out.println("3. Llenar cola");
+            System.out.println("4. Mostrar lista");
+            System.out.println("5. Mostrar pila");
+            System.out.println("6. Mostrar cola");
+            System.out.println("7. Modificar");
+            System.out.println("8. Eliminar");
+            System.out.println("9. Buscar por cedula");
+            System.out.println("10. Buscar por serial");
+            System.out.println("11. Volver al menu principal");
+
+            int opt = v.ValidarEntero(sc);
+            opt = v.ValidarRango(1, 11, opt, sc);
+
+            switch (opt) {
+                case 1:  lista = g.llenarLista(lista, sc);      break;
+                case 2:  pila  = g.llenarPila(pila, sc);        break;
+                case 3:  cola  = g.llenarCola(cola, sc);        break;
+                case 4:  g.mostrarLista(lista);                  break;
+                case 5:  g.mostrarPila(pila);                    break;
+                case 6:  g.mostrarCola(cola);                    break;
+                case 7:  lista = g.modificar(lista, sc);         break;
+                case 8:  lista = g.eliminar(lista, sc);          break;
+                case 9:  g.buscar(lista, sc);                    break;
+                case 10: g.buscarPorSerial(lista, sc);           break;
+                case 11: bandera = false;                        break;
+                default: System.out.println("Opcion no valida"); break;
             }
-        } while (opcion != 5);
+        }
     }
 }
